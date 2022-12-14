07b29fd0c55a071cf1256a53b8843419d4d5c3d5
Yingru Fang <fyr1116@gmail.com>
2022-12-13 16:53:32 -0800

    Updates

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import Home from "./Home";
-import About from "./About";
-import Project from "./Project";
-import Contact from "./Contact";
-import NavBar from "./NavBar";
-import Footer from "./Footer";
-
-export { Home, About, Project, Contact, NavBar, Footer };
